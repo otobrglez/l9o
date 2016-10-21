5072a4a1d18f06d913d4f68f0db1cb492269046d
Oto Brglez <otobrglez@gmail.com>
2016-10-21 02:15:51 +0200

    Adding video and gif cropping

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 .PHONY: all frames pngs
 text="univizor"
-font=whimsy
+# font=whimsy
+font=script
 
 out.gif: pngs
 	convert -loop 0 frames/*.png out.gif
 
 out.avi: pngs
-	./video.sh
+	ffmpeg -framerate 1/4 -r 30 -pattern_type glob -i 'frames/*.html.png' -vf scale=900:-1 -vcodec mpeg4 out.avi
 
 frames:
 	./frames.sh $(text) $(font)
@@ -20,3 +21,7 @@ clean:
 
 fontz:
 	artii -l | xargs -I{} sh -c 'echo {}; artii --font {} univizor > ./fontz/{}.txt'
+
+
+out-final.gif: out.gif
+	convert out.gif -crop "350x125+10+10" +repage out-final.gif
--- frames.sh
+++ frames.sh
@@ -9,7 +9,8 @@
 #done
 
 # Parallel version
-text=$1; font=$2 p=0; for i in $(seq -w 1 2 29 ; seq -w 29 -2 1); do p=$[$p+1]; printf "%02d-$i-$font-$text\n" $p; done | \
+# (seq -w 1 2 29 ; seq -w 29 -2 1)
+text=$1; font=$2 p=0; for i in $(seq -w 1 2 70 ; seq -w 70 -5 1); do p=$[$p+1]; printf "%02d-$i-$font-$text\n" $p; done | \
   parallel --progress -j 8 'f=$(echo {} | cut -d "-" -f1); s=$(echo {} | cut -d "-" -f2); \
     font=$(echo {} | cut -d "-" -f3); text=$(echo {} | cut -d "-" -f4); \
     artii -f $font "$text" | lolcat -S $s -p 2 -f | ansi2html -l -s solarized > ./frames/$f-$s.html'
